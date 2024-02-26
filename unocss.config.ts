51c68b622c1b0fcbcc19efe52950f19766cd634d
zhangxiaoqian <alexuzhang@gmail.com>
2024-02-26 15:56:13 +0800

    chore: add unocss

---
+++ unocss.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig, presetUno, presetAttributify, presetIcons } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons()
+    // ...
+  ],
+})
